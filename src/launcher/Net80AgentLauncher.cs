271d2223a14cb222a1482270c0c8854fe7f5925b
Charlie Poole <charliepoole@gmail.com>
2025-10-01 07:08:44 -0700

    Add AgentType and TargetFramework extension properties

--- src/launcher/Net80AgentLauncher.cs
+++ src/launcher/Net80AgentLauncher.cs
@@ -14,6 +14,8 @@ using NUnit.Extensibility;
 namespace NUnit.Engine.Agents
 {
     [Extension(Description = "Pluggable agent running tests under .NET 8.0", EngineVersion = "4.0.0")]
+    [ExtensionProperty("AgentType", "LocalProcess")]
+    [ExtensionProperty("TargetFramework", ".NETCoreApp,Version=8.0")]
     public class Net80AgentLauncher : IAgentLauncher
     {
         private static readonly string LAUNCHER_DIR = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
